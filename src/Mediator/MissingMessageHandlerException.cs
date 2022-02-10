2035ecf03ac92973616ce94899f31c564db50806
Martin Othamar <martin@othamar.net>
2022-02-10 20:19:07 +0100

    Simplify errorhandling

--- src/Mediator/MissingMessageHandlerException.cs
+++ src/Mediator/MissingMessageHandlerException.cs
@@ -4,20 +4,9 @@ namespace Mediator
 {
     public class MissingMessageHandlerException : Exception
     {
-        public IMessage? MediatorMessage { get; }
+        public object? MediatorMessage { get; }
 
-        public MissingMessageHandlerException(IMessage? message)
-            : base("No handler reqistered for message type: " + message?.GetType().FullName ?? "Unknown")
-        {
-            MediatorMessage = message;
-        }
-    }
-
-    public class MissingStreamMessageHandlerException : Exception
-    {
-        public IStreamMessage? MediatorMessage { get; }
-
-        public MissingStreamMessageHandlerException(IStreamMessage? message)
+        public MissingMessageHandlerException(object? message)
             : base("No handler reqistered for message type: " + message?.GetType().FullName ?? "Unknown")
         {
             MediatorMessage = message;
