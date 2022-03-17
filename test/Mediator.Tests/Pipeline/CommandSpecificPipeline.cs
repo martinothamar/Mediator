d26053c5fd0c5177daf56faf0edd738ea663c6b7
Martin Othamar <martin@othamar.net>
2022-03-17 22:24:28 +0100

    Command specific pipeline test

---
+++ test/Mediator.Tests/Pipeline/CommandSpecificPipeline.cs
@@ -0,0 +1,14 @@
+namespace Mediator.Tests.Pipeline;
+
+public sealed class CommandSpecificPipeline<TCommand, TResponse>
+    : IPipelineBehavior<TCommand, TResponse>
+    where TCommand : IBaseCommand
+{
+    public static int CallCount { get; private set; }
+
+    public ValueTask<TResponse> Handle(TCommand message, CancellationToken cancellationToken, MessageHandlerDelegate<TCommand, TResponse> next)
+    {
+        CallCount++;
+        return next(message, cancellationToken);
+    }
+}
