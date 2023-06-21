02e914bc7a94f0bf1808093fde343468baf990ed
Martin Othamar <martin@othamar.net>
2023-06-21 21:52:40 +0200

    rebase, some additional fixes

--- src/Mediator/IMediator.cs
+++ src/Mediator/IMediator.cs
@@ -2,7 +2,7 @@ namespace Mediator;
 
 /// <summary>
 /// Mediator instance for sending requests, commands, queries and their streaming counterparts (<see cref="IAsyncEnumerable{T}"/>).
-/// Also for publushing notifications.
+/// Also for publishing notifications.
 /// Use the concrete Mediator implementation for the highest performance (monomorphized method overloads per T available).
 /// Can use the <see cref="ISender"/> and <see cref="IPublisher"/> for requests/commands/queries and notifications respectively.
 /// </summary>
