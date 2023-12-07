ad0f48cbf0f3f2dbc4b240d2af5b9fc9927f9f18
Tim M <49349513+TimothyMakkison@users.noreply.github.com>
2023-12-07 22:52:09 +0000

    feat: add snapshot tests (#122)

---
+++ test/Mediator.SourceGenerator.Roslyn38.Tests/ModuleInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using VerifyTests;
+using VerifyXunit;
+
+namespace Mediator.SourceGenerator.Tests;
+
+public static class ModuleInitializer
+{
+    // ModuleInitializer should only be used in apps
+#pragma warning disable CA2255
+    [ModuleInitializer]
+#pragma warning restore CA2255
+    public static void Init()
+    {
+        Verifier.DerivePathInfo(
+            (file, _, type, method) => new(Path.Join(Path.GetDirectoryName(file), "_snapshots"), type.Name, method.Name)
+        );
+
+        VerifySourceGenerators.Initialize();
+        VerifyDiffPlex.Initialize(VerifyTests.DiffPlex.OutputType.Compact);
+    }
+}
